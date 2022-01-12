be39d61a9fac0bc3a77a24513f0512595a18ca35
SkyezerFox <actuallyori@gmail.com>
2022-01-12 14:28:44 +0000

    chore: remove .next folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
 	root: true,
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@next/next/recommended",
+		"prettier",
+	],
 	ignorePatterns: ["*.js"],
 };
