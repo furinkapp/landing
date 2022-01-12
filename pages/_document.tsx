2f6588b7e8cdf0da4210e26942f8392fc3bccb55
SkyezerFox <actuallyori@gmail.com>
2022-01-12 14:46:12 +0000

    feat: move links and add meta to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { DocumentContext } from "next/document";
+import Document, {
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +32,45 @@ export default class MyDocument extends Document {
 		} finally {
 			sheet.seal();
 		}
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<meta charSet="UTF-8" />
+
+					<meta
+						name="description"
+						content="Commission platform and artist network."
+					/>
+					<meta
+						name="keywords"
+						content="art, commission, platform, social media"
+					/>
+					<meta name="author" content="kaylen" />
+
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0"
+					/>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="*"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
+						rel="stylesheet"
+					/>
+					<title>fur.ink &middot; Coming Soon</title>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	}
 }
